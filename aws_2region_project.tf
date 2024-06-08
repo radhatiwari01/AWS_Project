c766c367013a442f1d4df054e9d047fa3f029c9c
Radha Tiwari <167222608+radhatiwari01@users.noreply.github.com>
2024-06-08 18:28:47 +0530

    Delete aws_2region_project.tf

--- aws_2region_project.tf
+++
@@ -1,9 +0,0 @@
-
-# Calling the modules to create EC2 instances
-module "ec2_instance_est01" {
-  source   = "./modules/ec2_instance_est01"
-}
-
-module "ec2_instance_est02" {
-  source  = "./modules/ec2_instance_est02"
-}
